f0d9186acfb7c543d85e71753ec41443447616a5
zahidupm <zahidhasan.limon111@gmail.com>
2023-08-05 10:28:26 +0600

    create role and permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +15,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $defaultPermissions = ['lead-management', 'create-admin', 'user-management'];
+        foreach ($defaultPermissions as $permission){
+            Permission::create(['name' => $permission]);
+        }
 
-         \App\Models\User::factory()->create([
-             'name' => 'Super Admin',
-             'email' => 'super-admin@permission.test',
-             'password' => bcrypt('123')
-         ]);
+        $this->create_user_with_role('Super Admin', 'Super Admin', 'super-admin@permission.test');
+        $this->create_user_with_role('Communication', 'Communication', 'communication@permission.test');
+        $this->create_user_with_role('Leads', 'Leads', 'leads@permission.test');
+    }
+
+    protected function create_user_with_role($type, $name, $email){
+        $role = Role::create([
+           'name' => $type,
+        ]);
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt('password')
+        ]);
+
+        if($type == 'Super Admin'){
+            $role->givePermissionTo(Permission::all());
+        } elseif($type == 'Leads'){
+            $role->givePermissionTo('lead-management');
+        }
+
+        $user->assignRole($role);
+
+        return $user;
     }
 }
